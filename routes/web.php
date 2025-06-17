2ed49e5f3e863a08fca69ddeb00d21b23f0844d3
endrico180 <theendrico@gmail.com>
2025-06-17 13:44:20 +0700

    add searh, filter, pagination using ajax on stock out menu

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('stock-in', StockInController::class)->middleware(['auth']);
     Route::get('/stock_in/search', [StockInController::class, 'search'])->name('stock_in.search');
     Route::resource('stock-out', StockOutController::class)->middleware(['auth']);
+    Route::get('/stock_out/search', [StockInController::class, 'search'])->name('stock_out.search');
     Route::get('/laporan', [ReportController::class, 'index'])->name('laporan')->middleware(['auth']);
 
     // Routes untuk profil
